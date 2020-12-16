ab977658c263f0c2747e7d2cd3453f1e296eb39a
Kevin Chaloux <kvnchaloux@gmail.com>
2020-12-16 17:05:25 -0500

    Load values from back-end to front-end

--- API/Startup.cs
+++ API/Startup.cs
@@ -24,6 +24,15 @@ namespace API
             {
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader()
+                        .AllowAnyMethod()
+                        .WithOrigins("http://localhost:3000");
+                });
+            });
             services.AddControllers();
         }
 
@@ -35,6 +44,7 @@ namespace API
                 app.UseDeveloperExceptionPage();
             }
             // app.UseHttpsRedirection();
+            app.UseCors("CorsPolicy");
             app.UseRouting();
             app.UseAuthorization();
             app.UseEndpoints(endpoints =>
